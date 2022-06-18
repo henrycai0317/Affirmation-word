59526cd910214bded1693f3136b5616879942a44
Henry Cai <alaba.havai@gmail.com>
2022-06-18 21:47:30 +0800

    Add Unit Test

---
+++ app/src/test/java/com/example/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,23 @@
+package com.example.affirmations
+
+import ItemAdapter
+import android.content.Context
+import com.example.affirmations.model.Affirmation
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class AffirmationsAdapterTests {
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size(){
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+}
